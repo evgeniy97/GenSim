c757627b8b41d0b6cec4d766d282991347638dbe
Евгений <evgen19-97@ya.ru>
2019-11-07 07:55:51 +0300

    update

--- utilities.py
+++ utilities.py
@@ -3,6 +3,21 @@ import matplotlib.pyplot as plt
 from Bio import Phylo
 from io import StringIO
 
+
+NAME_LENGTH = 4
+ALPHABET = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+MAX_START_POPULATION_SIZE = NAME_LENGTH**len(ALPHABET)
+
+
+def check_population_size(population_size):
+    if population_size > MAX_START_POPULATION_SIZE:
+        raise "You should change NAME_LENGTH"
+
+
+def generate_name(length=NAME_LENGTH):
+    return ''.join(np.random.choice(ALPHABET,size=NAME_LENGTH))
+
+
 def plot_tree(treedata, mutation_data ,output_file):
     #handle = StringIO(treedata)  # parse the newick string
     tree = Phylo.read(treedata, "newick")
@@ -15,5 +30,4 @@ def plot_tree(treedata, mutation_data ,output_file):
     plt.scatter(mutation_data['x'],mutation_data['y'])
     Phylo.draw(tree, axes=axes)
     plt.savefig(output_file, dpi=100)
-
     return
